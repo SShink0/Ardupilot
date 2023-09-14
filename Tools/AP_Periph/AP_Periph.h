c33a09aef29e31b72c324f5f8a2260ab8843be07
Peter Barker <pbarker@barker.dropbear.id.au>
2023-09-14 21:22:45 +1000

    AP_Periph: add missing SITL includes
    these are required for compilation when other things are introduced

--- Tools/AP_Periph/AP_Periph.h
+++ Tools/AP_Periph/AP_Periph.h
@@ -36,6 +36,9 @@
 #include <AP_RCProtocol/AP_RCProtocol_config.h>
 #include "rc_in.h"
 #include "batt_balance.h"
+#if AP_SIM_ENABLED
+#include <SITL/SITL.h>
+#endif
 
 #include <AP_NMEA_Output/AP_NMEA_Output.h>
 #if HAL_NMEA_OUTPUT_ENABLED && !(HAL_GCS_ENABLED && defined(HAL_PERIPH_ENABLE_GPS))
