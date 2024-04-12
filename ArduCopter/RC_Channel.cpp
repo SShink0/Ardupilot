91ea452fd5bcd5577bab336e3d1567bf190be271
Peter Barker <pbarker@barker.dropbear.id.au>
2024-04-12 12:50:30 +1000

    ArduCopter: add compile-time option to remove inverted-flight capability

--- ArduCopter/RC_Channel.cpp
+++ ArduCopter/RC_Channel.cpp
@@ -113,7 +113,9 @@ void RC_Channel_Copter::init_aux_function(const AUX_FUNC ch_option, const AuxSwi
     case AUX_FUNC::ACRO_TRAINER:
     case AUX_FUNC::ATTCON_ACCEL_LIM:
     case AUX_FUNC::ATTCON_FEEDFWD:
+#if AP_INVERTED_FLIGHT_ENABLED
     case AUX_FUNC::INVERTED:
+#endif
     case AUX_FUNC::MOTOR_INTERLOCK:
     case AUX_FUNC::PARACHUTE_3POS:      // we trust the vehicle will be disarmed so even if switch is in release position the chute will not release
     case AUX_FUNC::PARACHUTE_ENABLE:
@@ -412,7 +414,7 @@ bool RC_Channel_Copter::do_aux_function(const AUX_FUNC ch_option, const AuxSwitc
             break;
 #endif
 
-#if FRAME_CONFIG == HELI_FRAME
+#if AP_INVERTED_FLIGHT_ENABLED && FRAME_CONFIG == HELI_FRAME
         case AUX_FUNC::INVERTED:
             switch (ch_flag) {
             case AuxSwitchPos::HIGH:
@@ -430,7 +432,7 @@ bool RC_Channel_Copter::do_aux_function(const AUX_FUNC ch_option, const AuxSwitc
                 break;
             }
             break;
-#endif
+#endif  // AP_INVERTED_FLIGHT_ENABLED && FRAME_CONFIG == HELI_FRAME
 
 #if AP_WINCH_ENABLED
         case AUX_FUNC::WINCH_ENABLE:
